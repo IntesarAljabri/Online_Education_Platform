a74caa08abb4af15e41845504804a50bb6f90ec5
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-10 16:29:01 +0400

    NEW

--- Online_Education_Paltform/src/main/java/Online_Education_Paltform/MyProject/Controllers/QuizControllers.java
+++ Online_Education_Paltform/src/main/java/Online_Education_Paltform/MyProject/Controllers/QuizControllers.java
@@ -1,4 +1,24 @@
 package Online_Education_Paltform.MyProject.Controllers;
-
+import Online_Education_Paltform.MyProject.Models.Quiz;
+import Online_Education_Paltform.MyProject.Services.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping(value = "quiz")
 public class QuizControllers {
+
+    //    http://localhost:8080/quiz/getAll
+    @Autowired
+    QuizService quizservice;
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Quiz> getAllQuizzes() {
+
+        return quizservice.getAllQuizzes();
+    }
+    @GetMapping(value = "getById")
+    public Quiz getQuizById(@RequestParam Integer id) {
+
+        return quizservice.getQuizById(id);
+    }
 }
--- Online_Education_Paltform/src/main/java/Online_Education_Paltform/MyProject/Models/Assignment.java
+++ Online_Education_Paltform/src/main/java/Online_Education_Paltform/MyProject/Models/Assignment.java
@@ -1,4 +1,30 @@
 package Online_Education_Paltform.MyProject.Models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.*;
+import javax.persistence.*;
 
+@Getter
+@Setter
+@Data
+@Entity     
+@Table(name = "Assignment")
 public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id")
+    Integer assignment_id;
+
+    @Column(name = "assignment_name")
+    String assignment_name;
+
+    @ManyToOne
+    private Course course;
+
+    @OneToMany(mappedBy = "assignment")
+    @JsonIgnore
+    private List<Grade> grades;
 }
