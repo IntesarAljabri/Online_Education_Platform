147baa7769375801b04f04c1d3bea5658532e2e3
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-10 12:33:58 +0400

    Classes

---
+++ Online_Education_Paltform/src/test/java/Online_Education_Paltform/MyProject/OnlineEducationPaltformApplicationTests.java
@@ -0,0 +1,13 @@
+package Online_Education_Paltform.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEducationPaltformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
